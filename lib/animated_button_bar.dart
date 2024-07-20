b911b19c52c16bce51f5b80908bbcb883733087f
DonatelloX <donatello.amicone@gmail.com>
2024-07-20 18:14:06 +0200

    Update animated_button_bar.dart
    //initialIndex --> default button
    //ButtonBarEntry --> different color for each button

--- lib/animated_button_bar.dart
+++ lib/animated_button_bar.dart
@@ -1,12 +1,14 @@
-library animated_button_bar;
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+//initialIndex --> default button
+//ButtonBarEntry --> different color for each button
 
 ///A row of buttons with animated selection
 class AnimatedButtonBar extends StatefulWidget {
   ///Duration for the selection animation
   final Duration animationDuration;
+  final int initialIndex;
   final Color? backgroundColor;
   final Color? foregroundColor;
   final double radius;
@@ -27,9 +29,9 @@ class AnimatedButtonBar extends StatefulWidget {
   final AnimatedButtonController? controller;
 
   const AnimatedButtonBar({
-    Key? key,
+    super.key,
     required this.children,
-    this.animationDuration = const Duration(milliseconds: 200),
+    this.animationDuration = const Duration(milliseconds: 250),
     this.backgroundColor,
     this.foregroundColor,
     this.radius = 0.0,
@@ -41,25 +43,27 @@ class AnimatedButtonBar extends StatefulWidget {
     this.curve = Curves.fastOutSlowIn,
     this.padding = const EdgeInsets.all(0),
     this.invertedSelection = false,
-  }) : super(key: key);
+    this.initialIndex = 0,
+  });
 
   @override
   _AnimatedButtonBarState createState() => _AnimatedButtonBarState();
 }
 
 class _AnimatedButtonBarState extends State<AnimatedButtonBar> {
-
   late AnimatedButtonController _controller;
+
   @override
   void initState() {
     _controller = widget.controller ?? AnimatedButtonController();
+    _controller.setIndex(widget.initialIndex);
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     Color backgroundColor =
-        widget.backgroundColor ?? Theme.of(context).colorScheme.background;
+        widget.backgroundColor ?? Theme.of(context).colorScheme.surface;
     return ChangeNotifierProvider(
       create: (context) => _controller,
       child: Consumer<AnimatedButtonController>(
@@ -67,79 +71,83 @@ class _AnimatedButtonBarState extends State<AnimatedButtonBar> {
           return Padding(
             padding: widget.padding,
             child: LayoutBuilder(
-                builder: (BuildContext context, BoxConstraints constraints) {
-              return Card(
-                color: backgroundColor,
-                shape: RoundedRectangleBorder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                return Card(
+                  color: backgroundColor,
+                  shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(widget.radius)),
                     side: BorderSide(
                       color: widget.borderColor ?? Colors.transparent,
                       width: widget.borderWidth ??
                           (widget.borderColor != null ? 1.0 : 0.0),
-                    )),
-                elevation: widget.elevation,
-                child: Stack(
-                  fit: StackFit.loose,
-                  children: [
-                    AnimatedPositioned(
-                      top: 0,
-                      bottom: 0,
-                      left: constraints.maxWidth / widget.children.length * animatedButton.index,
-                      right: (constraints.maxWidth / widget.children.length) *
-                          (widget.children.length - animatedButton.index - 1),
-                      duration: widget.animationDuration,
-                      curve: widget.curve,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color:
-                              widget.foregroundColor ?? Theme.of(context).colorScheme.secondary,
-                          borderRadius:
-                              BorderRadius.all(Radius.circular(widget.radius)),
+                    ),
+                  ),
+                  elevation: widget.elevation,
+                  child: Stack(
+                    fit: StackFit.loose,
+                    children: [
+                      AnimatedPositioned(
+                        top: 0,
+                        bottom: 0,
+                        left: constraints.maxWidth / widget.children.length * animatedButton.index,
+                        right: (constraints.maxWidth / widget.children.length) *
+                            (widget.children.length - animatedButton.index - 1),
+                        duration: widget.animationDuration,
+                        curve: widget.curve,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: widget.children[animatedButton.index].color ??
+                                widget.foregroundColor ??
+                                Theme.of(context).colorScheme.secondary,
+                            borderRadius: BorderRadius.all(Radius.circular(widget.radius)),
+                          ),
                         ),
                       ),
-                    ),
-                    Row(
-                      children: widget.children
-                          .asMap()
-                          .map((i, sideButton) => MapEntry(
-                                i,
-                                Expanded(
-                                  child: InkWell(
-                                    onTap: () {
-                                      try {
-                                        sideButton.onTap();
-                                      } catch (e) {
-                                        print('onTap implementation is missing');
-                                      }
-                                      animatedButton.setIndex(i);
-                                    },
-                                    borderRadius: BorderRadius.all(
-                                        Radius.circular(widget.radius)),
-                                    child: Padding(
-                                      padding: EdgeInsets.symmetric(
-                                          vertical: widget.innerVerticalPadding),
-                                      child: Center(
-                                          child: ColorFiltered(
-                                              colorFilter: ColorFilter.mode(
-                                                  backgroundColor,
-                                                  widget.invertedSelection &&
-                                                      animatedButton.index == i
-                                                      ? BlendMode.srcIn
-                                                      : BlendMode.dstIn),
-                                              child: sideButton.child)),
+                      Row(
+                        children: widget.children
+                            .asMap()
+                            .map((i, sideButton) => MapEntry(
+                          i,
+                          Expanded(
+                            child: InkWell(
+                              onTap: () {
+                                try {
+                                  sideButton.onTap();
+                                } catch (e) {
+                                  print('onTap implementation is missing');
+                                }
+                                animatedButton.setIndex(i);
+                              },
+                              borderRadius: BorderRadius.all(Radius.circular(widget.radius)),
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(vertical: widget.innerVerticalPadding),
+                                child: Center(
+                                  child: ColorFiltered(
+                                    colorFilter: ColorFilter.mode(
+                                        animatedButton.index == i
+                                            ? (widget.invertedSelection ? backgroundColor : (sideButton.color ?? widget.foregroundColor ?? Theme.of(context).colorScheme.secondary))
+                                            : backgroundColor,
+                                        animatedButton.index == i && widget.invertedSelection
+                                            ? BlendMode.srcIn
+                                            : BlendMode.dstIn
                                     ),
+                                    child: sideButton.child,
                                   ),
                                 ),
-                              ))
-                          .values
-                          .toList(),
-                    ),
-                  ],
-                ),
-              );
-            }),
+                              ),
+                            ),
+                          ),
+                        ))
+                            .values
+                            .toList(),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
           );
-        }
+        },
       ),
     );
   }
@@ -148,12 +156,12 @@ class _AnimatedButtonBarState extends State<AnimatedButtonBar> {
 class ButtonBarEntry {
   final Widget child;
   final VoidCallback onTap;
-  ButtonBarEntry({required this.child, required this.onTap});
+  final Color? color; // Nuovo parametro per il colore
+  ButtonBarEntry({required this.child, required this.onTap, this.color});
 }
 
 ///controller for AnimatedButtonBar widget
 class AnimatedButtonController extends ChangeNotifier {
-
   int index = 0;
 
   ///change the index programmatically
